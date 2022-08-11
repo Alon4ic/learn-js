dd2450bd189b7fff2460755c6bd5a5b7358b9f86
Alon4ic <o.redjko@gmail.com>
2022-08-11 15:55:59 +0300

    Save changes

--- js/script.js
+++ js/script.js
@@ -221,11 +221,53 @@
 // console.log(typeof(str));
 // console.log(typeof(strObj));
 
-console.dir([1,2,3]);
-const soldier = {
-	health: 400,
-	armor: 100
-};
-const jonh = {
-	health: 100
-};
+// console.dir([1,2,3]);
+// const soldier = {
+// 	health: 400,
+// 	armor: 100
+// };
+// const jonh = {
+// 	health: 100
+// };
+
+// const getPeople = count =>
+// new Promise((resolves, rejects) => {
+// const api = `https://api.randomuser.me/?nat=US&results=${count}`; const request = new XMLHttpRequest();
+// request.open("GET", api);
+// request.onload = () =>
+// request.status === 200
+// ? resolves(JSON.parse(request.response).results) : reject(Error(request.statusText));
+// request.onerror = err => rejects(err);
+// request.send(); });
+// getPeople(5)
+// .then(members => console.log(members))
+// .catch(error => console.error(`getPeople failed: ${error.message}`)) );
+
+
+// function Vacation(destination, length) { this.destination = destination;
+// this.length = length; }
+// Vacation.prototype.print = function() {
+// console.log(this.destination + " | " + this.length + " days"); };
+// const maui = new Vacation("Maui", 7);
+// maui.print();
+
+
+class Vacation { constructor(destination, length) {
+this.destination = destination;
+this.length = length; }
+print() {
+console.log(`${this.destination} will take ${this.length} days.`); }
+}
+// const trip = new Vacation("Santiago, Chile", 7);
+// trip.print(); 
+class Expedition extends Vacation { constructor(destination, length, gear) {
+super(destination, length);
+this.gear = gear; }
+print() { super.print();
+console.log(`Bring your ${this.gear.join(" and your ")}`); }
+}
+const trip = new Expedition("Mt. Whitney", 3, [ "sunglasses",
+"prayer flags",
+"camera"
+]); 
+trip.print();
